0d83547fa5ad30655e2de32a45d315a9f21565dd
Minji Kim <minjikim2000@hotmail.com>
2022-12-26 02:39:43 +0900

    1000

--- io_1.py
+++ io_1.py
@@ -1,3 +1,18 @@
-#2557번 - Hello world
+#1 단계 - 입출력과 사칙연산
 
+#2557번 - Hello world
 print("Hello World!")
+
+#1000번 - A+B
+#런타임 에러(ValueError)
+#a = int(input())
+#b = int(input())
+
+#print(a+b)
+
+#정답
+a, b = input().split()
+print(int(a) + int(b))
+
+a,b = map(int, input().split())
+print(a+b)
