ef0521feca48ef9e9c0fb408ba931b8fc648eb76
Minji Kim <minjikim2000@hotmail.com>
2023-01-03 01:47:38 +0900

    9498

--- conditional_2.py
+++ conditional_2.py
@@ -9,3 +9,17 @@ elif a < b:
     print("<")
 else :
     print("==")
+
+#9498번 - 시험 성적
+score = int(input())
+
+if score >= 90 :
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
