5e9e9ef526dc58a5fb3c2402334b4e34f230e1d6
Minji Kim <minjikim2000@hotmail.com>
2023-01-04 12:05:50 +0900

    2753

--- conditional_2.py
+++ conditional_2.py
@@ -23,3 +23,11 @@ elif score >= 60:
     print("D")
 else:
     print("F")
+
+#2753번 - 윤년
+l_year = int(input())
+
+if (l_year % 4 == 0 and l_year % 100 != 0) or (l_year % 400 == 0) :
+    print(1)
+else :
+    print(0)
